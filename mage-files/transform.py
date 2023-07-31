d962d413c8c27628c4e832b64ebbfd9054f9aaad
Soham Desai <100659435+SohamDesai27@users.noreply.github.com>
2023-07-31 16:39:28 -0700

    Update transform.py

--- mage-files/transform.py
+++ mage-files/transform.py
@@ -1,10 +1,14 @@
+# Import necessary libraries
 import pandas as pd
+
+# Check if 'transformer' and 'test' functions are not already in the global scope, and if not, import them.
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
-
+# Define a transformer function using the 'transformer' decorator.
+# This function performs data transformation on the input DataFrame 'df'.
 @transformer
 def transform(df, *args, **kwargs):
     """
@@ -14,16 +18,19 @@ def transform(df, *args, **kwargs):
     There should be one parameter for each output variable from each parent block.
 
     Args:
-        data: The output from the upstream parent block
+        df: The input DataFrame containing the data to be transformed.
         args: The output from any additional upstream blocks (if applicable)
 
     Returns:
-        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+        A dictionary containing the transformed data.
     """
     # Specify your transformation logic here
+
+    # Convert the 'tpep_pickup_datetime' and 'tpep_dropoff_datetime' columns to pandas datetime objects.
     df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
     df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
 
+    # Create a new DataFrame 'datetime_dim' to store date and time-related dimensions from the input DataFrame.
     datetime_dim = df[['tpep_pickup_datetime','tpep_dropoff_datetime']].drop_duplicates().reset_index(drop=True)
     datetime_dim['pick_hour'] = datetime_dim['tpep_pickup_datetime'].dt.hour
     datetime_dim['pick_day'] = datetime_dim['tpep_pickup_datetime'].dt.day
@@ -41,50 +48,59 @@ def transform(df, *args, **kwargs):
     datetime_dim = datetime_dim[['datetime_id', 'tpep_pickup_datetime', 'pick_hour', 'pick_day', 'pick_month', 'pick_year', 'pick_weekday',
                              'tpep_dropoff_datetime', 'drop_hour', 'drop_day', 'drop_month', 'drop_year', 'drop_weekday']]
 
+    # Create 'passenger_count_dim' DataFrame to store passenger count-related dimensions.
     passenger_count_dim = df[['passenger_count']].drop_duplicates().reset_index(drop=True)
     passenger_count_dim['passenger_count_id'] = passenger_count_dim.index
     passenger_count_dim = passenger_count_dim[['passenger_count_id','passenger_count']]
 
+    # Create 'trip_distance_dim' DataFrame to store trip distance-related dimensions.
     trip_distance_dim = df[['trip_distance']].drop_duplicates().reset_index(drop=True)
     trip_distance_dim['trip_distance_id'] = trip_distance_dim.index
     trip_distance_dim = trip_distance_dim[['trip_distance_id','trip_distance']]
+
+    # Define a dictionary to map rate code IDs to rate code names.
     rate_code_type = {
-        1:"Standard rate",
-        2:"JFK",
-        3:"Newark",
-        4:"Nassau or Westchester",
-        5:"Negotiated fare",
-        6:"Group ride"
+        1: "Standard rate",
+        2: "JFK",
+        3: "Newark",
+        4: "Nassau or Westchester",
+        5: "Negotiated fare",
+        6: "Group ride"
     }
 
+    # Create 'rate_code_dim' DataFrame to store rate code-related dimensions.
     rate_code_dim = df[['RatecodeID']].drop_duplicates().reset_index(drop=True)
     rate_code_dim['rate_code_id'] = rate_code_dim.index
     rate_code_dim['rate_code_name'] = rate_code_dim['RatecodeID'].map(rate_code_type)
     rate_code_dim = rate_code_dim[['rate_code_id','RatecodeID','rate_code_name']]
 
-
+    # Create 'pickup_location_dim' DataFrame to store pickup location-related dimensions.
     pickup_location_dim = df[['pickup_longitude', 'pickup_latitude']].drop_duplicates().reset_index(drop=True)
     pickup_location_dim['pickup_location_id'] = pickup_location_dim.index
     pickup_location_dim = pickup_location_dim[['pickup_location_id','pickup_latitude','pickup_longitude']] 
 
-
+    # Create 'dropoff_location_dim' DataFrame to store dropoff location-related dimensions.
     dropoff_location_dim = df[['dropoff_longitude', 'dropoff_latitude']].drop_duplicates().reset_index(drop=True)
     dropoff_location_dim['dropoff_location_id'] = dropoff_location_dim.index
     dropoff_location_dim = dropoff_location_dim[['dropoff_location_id','dropoff_latitude','dropoff_longitude']]
 
+    # Define a dictionary to map payment types IDs to payment type names.
     payment_type_name = {
-        1:"Credit card",
-        2:"Cash",
-        3:"No charge",
-        4:"Dispute",
-        5:"Unknown",
-        6:"Voided trip"
+        1: "Credit card",
+        2: "Cash",
+        3: "No charge",
+        4: "Dispute",
+        5: "Unknown",
+        6: "Voided trip"
     }
+
+    # Create 'payment_type_dim' DataFrame to store payment type-related dimensions.
     payment_type_dim = df[['payment_type']].drop_duplicates().reset_index(drop=True)
     payment_type_dim['payment_type_id'] = payment_type_dim.index
     payment_type_dim['payment_type_name'] = payment_type_dim['payment_type'].map(payment_type_name)
     payment_type_dim = payment_type_dim[['payment_type_id','payment_type','payment_type_name']]
 
+    # Create the 'fact_table' DataFrame that represents the central fact table containing all the dimensions and measures.
     fact_table = df.merge(passenger_count_dim, on='passenger_count') \
              .merge(trip_distance_dim, on='trip_distance') \
              .merge(rate_code_dim, on='RatecodeID') \
@@ -97,19 +113,6 @@ def transform(df, *args, **kwargs):
                'payment_type_id', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
                'improvement_surcharge', 'total_amount']]
 
+    # Return a dictionary containing all the transformed data.
     return {"datetime_dim":datetime_dim.to_dict(orient="dict"),
-    "passenger_count_dim":passenger_count_dim.to_dict(orient="dict"),
-    "trip_distance_dim":trip_distance_dim.to_dict(orient="dict"),
-    "rate_code_dim":rate_code_dim.to_dict(orient="dict"),
-    "pickup_location_dim":pickup_location_dim.to_dict(orient="dict"),
-    "dropoff_location_dim":dropoff_location_dim.to_dict(orient="dict"),
-    "payment_type_dim":payment_type_dim.to_dict(orient="dict"),
-    "fact_table":fact_table.to_dict(orient="dict")}
-
-
-@test
-def test_output(output, *args) -> None:
-    """
-    Template code for testing the output of the block.
-    """
-    assert output is not None, 'The output is undefined'
+            "
